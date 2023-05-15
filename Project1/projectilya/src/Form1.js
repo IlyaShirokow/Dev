1c08b886f17692f96b3f26c31817a2d7daea352e
ilya <eleiya62@gmail.com>
2023-05-15 20:22:32 +0300

    linked back and front

--- Project1/projectilya/src/Form1.js
+++ Project1/projectilya/src/Form1.js
@@ -1,59 +1,74 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { UseFrom } from 'react-hook-form';
-import { Button, Modal, Form, input } from 'react-bootstrap';
+import { Button,Form } from 'react-bootstrap';
 
 
-export const Form1 = () => (
+export class Form1 extends Component {
     
+static displayName = Form1.name;
+
+    constructor(props){
+
+        super(props);
+        this.state = {
+            email :'',
+            phone : '',
+            nameorg:'',
+            Id :''
+        }
+
+       
+        this.onInputChange = this.onInputChange.bind(this);
+        this.sendOrganizationsData = this.sendOrganizationsData.bind(this);
+
+    }
+
+    onInputChange(event) {
+        this.setState({
+            [event.target.name]: event.target.value
+        });
+    }
     
-    <>
+    render() {
+
+        return (
  
-        <h1 style ={{padding:'10px'}}>Форма для заполнения</h1>
+        <><h1 style ={{padding:'10px'}}>Форма для заполнения</h1>
      
         <Form style ={{padding:'10px'}}>
             
 
             <div class="form-row">
                 <div class="form-group col-md-6">
-                    <label for="inputName">ФИО</label>
-                    <input type="Name" class="form-control" id="inputFIO" placeholder="" />
+                    <label for="inputName">Почта</label>
+                    <input type="Text" name="email" class="form-control" id="inputFIO" placeholder="" />
                     
                 </div>
                 <div class="form-group col-md-6">
+                    <label for="inputPassword4">ID</label>
+                    <input name="Id" class="form-control" id="inputPhone" />
+                </div>
+                <div class="form-group col-md-6">
                     <label for="inputPassword4">Телефон</label>
-                    <input class="form-control" id="inputPhone" />
+                    <input name="phone" class="form-control" id="inputPhone" />
                 </div>
             </div>
             <div class="form-group col-md-6">
-                <label for="inputAddress">Адрес</label>
-                <input type="text" class="form-control" id="inputAddress"  />
+                <label for="inputAddress">Название организации</label>
+                <input type="text" name="nameorg" class="form-control" id="inputAddress"  />
             </div>
             <div class="form-row">
-                <div class="form-group col-md-6">
-                    <label for="inputCity">ИНН</label>
-                    <input type="text" class="form-control" id="inputINN" />
-                </div>
                 <div class="form-group col-md-4">
                     <label for="inputProduct">Продукт</label>
                     <select id="inputState" class="form-control">
                         <option selected>Выбрать</option>
-                        <option>Предзаказ Iphone 15 Pro</option>
-                        <option>Телефон Xiaomi 13 lite</option>
-                        <option>Телефон Xiaomi 10s</option>
-                        <option>Телефон Redmi not 12 pro+</option>
-                        <option>Телефон Xiaomi 12 Ultra</option>
-                        <option>Ноутбук Asus Predator 300</option>
-                        <option>Ноутбук HP-Spectre</option>
-                        <option>Ноутбук HP Ultimate Edition+</option>
-                        <option>Ноутбук MacBook Pro</option>
-                        <option>Телевизор Econ EX-32HS019B</option>
-                        <option>Телевизор Samsung Neo 2</option>
-                        <option>Телевизор Novex NWX-75U011MSG</option>
-                        <option>Телевизор Misura t9</option>
-                        <option>Игровая приставка Playstation 5</option>
-                        <option>Игровая приставка  Xbox one s</option>
-                        <option>Игровая приставка  Nintendo Switch</option>
-                        <option>Игровая приставка  Steam Deck</option>
+                        <option>Сухарики 3 корочки </option>
+                        <option>Пиво старый мельник</option>
+                        <option>Чипсы Lays</option>
+                        <option> Кока кола </option>
+                        <option> Фанта </option>
+                        <option> Тушеная говядина </option>
+                        
                     </select>
                 </div>
 
@@ -63,9 +78,34 @@ export const Form1 = () => (
             <button  type="submit" class="f-button f-button - warning" >Продолжить</button>
 
         </Form>
+        </>
 
+        );
+    }  
+    
+    async sendOrganizationsData() {
+        let Organizations = {
+            "Guid Id": this.state.Id,
+            "NameOrganizations": this.state.nameorg,
+            " Email ": this.state.email,
+            "PhoneNumber": this.state.phone
+            
+        };
+        console.log(Organizations);
+
+        const reponse = await fetch('https://localhost:7166/Organizations/create', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                
+            },
+            body: JSON.stringify(Organizations)
+        });
+
+        const data = await reponse.json();
+        console.log(data);
         
+ }  
 
-    </>
-)
-export default Form1;
+}
+
